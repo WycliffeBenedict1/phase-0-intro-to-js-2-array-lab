274a7dec498a42c169d29158c34a6465642163ad
WycliffeBenedict1 <wycliffebenedict24@gmail.com>
2025-01-14 13:23:20 +0300

    array intro

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Bella){
+    cats.push(Bella);
+}
+
+function destructivelyPrependCat(Elsa) {
+    cats.unshift(Elsa);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat( Broom){
+    return [...cats, "Broom"];
+}
+
+function prependCat(Arnold) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+    return cats.slice (1)
+}
